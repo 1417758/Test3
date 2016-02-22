d2d4bd1af7c20b6c7a236aa637ec95c81a739584
1417758 <1417758@rgu.ac.uk>
2016-02-22 10:31:03 +0000

    asdgsad

---
+++ Shoes/default.php
@@ -0,0 +1,32 @@
+<?php
+include("header.php");
+?>
+<!--START OF MAIN -->
+<main>
+    <section>
+        <figure id="figHeader">
+            <img src="../Images/nice.jpg">
+        </figure>
+    </section>
+
+    <aside>
+        <fieldset>
+            <legend>Shoes</legend>
+            <p>Global Event Attributes
+                HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global event attributes that can be added to HTML elements to define event actions.
+                = New event attributes in HTML5.</p>
+        </fieldset>
+    </aside>
+</main>
+<!--END OF MAIN -->
+
+<!--START OF FOOTER -->
+<footer>
+    <br/><hr>
+    <summary>&copy; 2016 Rachie Holdings. All rights reserved.</summary>
+</footer>
+<!--END OF FOOTER -->
+</body>
+</html>
---
+++ Shoes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Magical Shoes!</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/htmlDefault.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/menu.css">
+</head>
+<body>
+<!--START OF HEADER -->
+<header>
+    <section>
+        <figure id="figHeader">
+            <img src="../Images/many.jpg" alt="The Pulpit Rock" width="304" height="128px">
+        </figure>
+        <h1>The Magical world of shoes!</h1>
+        <marquee> <time datetime="YYYY-MM-DDThh:mm:ssTZD">now</time></marquee>
+    </section>
+    <div id="wrapper">
+        <div id="revised-header">
+            <div id="revised-main-nav">
+                <ul>
+                    <li> <a href="history.html">The History of shoes</a> </li>
+                    <li> <a href="favourite.html">My favourite shoes</a></li>
+                    <li> <a href="jokes.html">the best shoes jokes</a></li>
+                    <li><a href="help.html">Help with shoes</a></li>
+                </ul>
+            </div>
+        </div></div>
+</header>
+<!--END OF HEADER -->
