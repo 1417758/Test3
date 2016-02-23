0d1ea697003b0489f854678274a607b51f06595e
1417758 <1417758@rgu.ac.uk>
2016-02-23 11:40:18 +0000

    asdgsad

--- Shoes2/jokes.php
+++ Shoes2/jokes.php
@@ -5,48 +5,45 @@ include("header.php");
     <main>
         <h2>The best shoes jokes</h2>
         <article id="article1">
-            <header><h3>sandals</h3>
+            <header> <h3>sandals</h3>
             </header>
             <main>
                 <figure id="article1Fig">
-                    <img src="../Images/nice2.jpg">
+                    <img src="../Images/nice2.jpg" >
                 </figure>
             </main>
             <footer>
-                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
-                    a user clicks on an element.
+                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a user clicks on an element.
                     To learn more about programming events, please visit our JavaScript tutorial.
                     Below are the global</p>
             </footer>
         </article>
         <div id="articleMiddle2">
-            <article id="articleMiddle" style="background-color: crimson; width: 30%; max-width: 150px; height: 550px; display: inline;">
-                <header><h3>trainers</h3>
-                </header>
-                <main>
-                    <figure id="article2Fig">
-                        <img src="../Images/sleepers.jpg">
-                    </figure>
-                </main>
-                <footer style="background-color: chartreuse; ">
-                    <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript
-                        when a user clicks on an element.
-                        To learn more about programming events, please visit our JavaScript tutorial.
-                        Below are the global</p>
-                </footer>
-            </article>
+        <article id="articleMiddle" style="background-color: crimson; ">
+            <header> <h3>trainers</h3>
+            </header>
+            <main>
+                <figure id="article2Fig">
+                    <img src="../Images/sleepers.jpg" >
+                </figure>
+            </main>
+            <footer style="background-color: chartreuse; ">
+                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a user clicks on an element.
+                    To learn more about programming events, please visit our JavaScript tutorial.
+                    Below are the global</p>
+            </footer>
+        </article>
         </div>
         <article id="article3">
-            <header><h3>high heels</h3>
+            <header> <h3>high heels</h3>
             </header>
             <main>
                 <figure id="article3Fig">
-                    <img src="../Images/nice.jpg">
+                    <img src="../Images/nice.jpg" >
                 </figure>
             </main>
             <footer>
-                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
-                    a user clicks on an element.
+                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a user clicks on an element.
                     To learn more about programming events, please visit our JavaScript tutorial.
                     Below are the global</p>
             </footer>
