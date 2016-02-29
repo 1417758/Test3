8491d68ee2b428f4e2630b4883b276ef2d432c9e
1417758 <1417758@rgu.ac.uk>
2016-02-29 10:08:50 +0000

    asdgsad

--- Shoes2/header.php
+++ Shoes2/header.php
@@ -17,7 +17,7 @@
         <figure id="figHeader" class="grid-35" style="background-color: lightpink">
             <img src="../Images/many.jpg" alt="headerShoes">
         </figure>
-        <div class="grid-65" style="background-color:greenyellow"><h1>The Magical world of shoes!</h1></div>
+        <div class="grid-60" style="background-color:greenyellow"><h1>The Magical world of shoes!</h1></div>
 
         <br>
 
