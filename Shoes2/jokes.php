2ef611f82512c4d8cdeb2ede67caeb355d3bef6d
1417758 <1417758@rgu.ac.uk>
2016-02-29 09:50:48 +0000

    asdgsad

--- Shoes2/jokes.php
+++ Shoes2/jokes.php
@@ -4,6 +4,7 @@ include("header.php");
     <!--START OF MAIN -->
     <main class="grid-container">
         <h2>The best shoes jokes</h2>
+
         <section id="article1" class="grid-33 tablet-grid-50 mobile-grid-100">
             <header><h3>sandals</h3>
             </header>
@@ -19,23 +20,23 @@ include("header.php");
                     Below are the global</p>
             </footer>
         </section>
-        <div id="articleMiddle2" class="grid-33 tablet-grid-50 mobile-grid-100">
-            <section id="articleMiddle" style="background-color: crimson; ">
-                <header><h3>trainers</h3>
-                </header>
-                <main>
-                    <figure id="article2Fig">
-                        <img src="../Images/sleepers.jpg">
-                    </figure>
-                </main>
-                <footer style="background-color: chartreuse; ">
-                    <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript
-                        when a user clicks on an element.
-                        To learn more about programming events, please visit our JavaScript tutorial.
-                        Below are the global</p>
-                </footer>
-            </section>
-        </div>
+
+
+        <section id="article2" class="grid-33 tablet-grid-50 mobile-grid-100">
+            <header><h3>trainers</h3></header>
+            <main>
+                <figure id="article2Fig">
+                    <img src="../Images/sleepers.jpg">
+                </figure>
+            </main>
+            <footer>
+                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript
+                    when a user clicks on an element.
+                    To learn more about programming events, please visit our JavaScript tutorial.
+                    Below are the global</p>
+            </footer>
+        </section>
+
         <section id="article3" class="grid-33 tablet-grid-50 mobile-grid-100">
             <header><h3>high heels</h3>
             </header>
