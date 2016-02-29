e70e361b60e587337a2e38ed58256f0bb2277096
1417758 <1417758@rgu.ac.uk>
2016-02-29 09:46:00 +0000

    asdgsad

--- Shoes2/jokes.php
+++ Shoes2/jokes.php
@@ -4,7 +4,7 @@ include("header.php");
     <!--START OF MAIN -->
     <main class="grid-container">
         <h2>The best shoes jokes</h2>
-        <section id="article1">
+        <section id="article1" class="grid-33 tablet-grid-50 mobile-grid-100">
             <header><h3>sandals</h3>
             </header>
             <main>
@@ -19,7 +19,7 @@ include("header.php");
                     Below are the global</p>
             </footer>
         </section>
-        <div id="articleMiddle2">
+        <div id="articleMiddle2" class="grid-33 tablet-grid-50 mobile-grid-100">
             <section id="articleMiddle" style="background-color: crimson; ">
                 <header><h3>trainers</h3>
                 </header>
@@ -36,7 +36,7 @@ include("header.php");
                 </footer>
             </section>
         </div>
-        <section id="article3">
+        <section id="article3" class="grid-33 tablet-grid-50 mobile-grid-100">
             <header><h3>high heels</h3>
             </header>
             <main>
