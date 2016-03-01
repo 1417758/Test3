87298185ac7193fcdc82836423f609544f78dc74
1417758 <1417758@rgu.ac.uk>
2016-03-01 10:05:17 +0000

    asdgsad

--- AssesLabMOCK/default.php
+++ AssesLabMOCK/default.php
@@ -3,10 +3,34 @@ include("rguHeader.php");
 ?>
 <!--START OF MAIN -->
 <main class="grid-container">
-    <section class="grid-30">
-        <figure id="figHeader">
-            <img src="../Images/nice.jpg">
-        </figure>
+
+    <section class="grid-66">
+        <img src="../Images/nice.jpg">
+        <h1>Welcome to the School of Computing and Digital media</h1>
+        <p>Global Event Attributes
+            HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a
+            user clicks on an element.
+            To learn more about programming events, please visit our JavaScript tutorial.
+            Below are the global event attributes that can be added to HTML elements to define event actions.
+            = New event attributes in HTML5.</p>
+        <article id="article1">
+            <h2>Lorern Ipsum Subittle Text</h2>
+            <p>Global Event Attributes
+                HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a
+                user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global event attributes that can be added to HTML elements to define event actions.
+                = New event attributes in HTML5.</p>
+        </article>
+        <article id="article2">
+            <h2>Lorern Ipsum Subittle Text</h2>
+            <p>Global Event Attributes
+                HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a
+                user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global event attributes that can be added to HTML elements to define event actions.
+                = New event attributes in HTML5.</p>
+        </article>
     </section>
     <br/>
     <aside class="grid-70">
@@ -14,7 +38,8 @@ include("rguHeader.php");
         <fieldset>
             <legend>Shoes</legend>
             <p>Global Event Attributes
-                HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a user clicks on an element.
+                HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a
+                user clicks on an element.
                 To learn more about programming events, please visit our JavaScript tutorial.
                 Below are the global event attributes that can be added to HTML elements to define event actions.
                 = New event attributes in HTML5.</p>
