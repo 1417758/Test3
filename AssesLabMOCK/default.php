31781c9b8f664f4f3bbf31ba8121e83d342840bd
1417758 <1417758@rgu.ac.uk>
2016-03-01 11:40:20 +0000

    asdgsad

--- AssesLabMOCK/default.php
+++ AssesLabMOCK/default.php
@@ -5,16 +5,19 @@ include("rguHeader.php");
 <main class="grid-container">
 
     <section id="mainSection" class="grid-66 tablet-grid-50 mobile-grid-100">
-        <img id="wifie" class="grid-33" src="assets/chrisinajayne.jpg" >
+        <article id="article1">
+            <img id="wifie" class="grid-33" src="assets/chrisinajayne.jpg">
 
-        <h1>Welcome to the School of Computing and Digital media</h1>
+            <h1>Welcome to the School of Computing and Digital media</h1>
 
-        Global Event Attributes HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a
+            Global Event Attributes HTML 4 added the ability to let events trigger actions in a browser, like starting a
+            JavaScript when a
             user clicks on an element.
             To learn more about programming events, please visit our JavaScript tutorial.
             Below are the global event attributes that can be added to HTML elements to define event actions.
             = New event attributes in HTML5.
-        <article id="article1">
+        </article>
+        <article id="article2">
             <h3>Lorern Ipsum Subittle Text</h3>
 
             <p>Global Event Attributes
@@ -24,7 +27,7 @@ include("rguHeader.php");
                 Below are the global event attributes that can be added to HTML elements to define event actions.
                 = New event attributes in HTML5.</p>
         </article>
-        <article id="article2">
+        <article id="article3">
             <h3>Lorern Ipsum Subittle Text</h3>
 
             <p>Global Event Attributes
@@ -37,7 +40,7 @@ include("rguHeader.php");
     </section>
 
     <aside class="grid-33" class="grid-33 tablet-grid-30 mobile-grid-100">
-        <section id="article1Side" >
+        <section id="article1Side">
             <header><h3>Latest News</h3>
             </header>
             <main>
