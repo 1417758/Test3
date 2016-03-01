8f05e2380365e5f79241fd5a63747aeae155e42d
1417758 <1417758@rgu.ac.uk>
2016-03-01 12:14:30 +0000

    asdgsad

--- AssesLabMOCK/default.php
+++ AssesLabMOCK/default.php
@@ -7,6 +7,7 @@ include("rguHeader.php");
     <section id="mainSection" class="grid-66 tablet-grid-66 mobile-grid-66">
         <article id="article1">
             <img id="wifie" class="grid-33" src="assets/chrisinajayne.jpg">
+
             <h1>Welcome to the School of Computing and Digital media</h1>
             <span>Global Event Attributes HTML 4 added the ability to let events trigger actions in a browser, like starting a
             JavaScript when a
@@ -35,39 +36,39 @@ include("rguHeader.php");
         </article>
     </section>
 
-    <aside class="grid-33" class="grid-33 tablet-grid-33 mobile-grid-33" ">
-        <section id="article1Side">
-            <header><h3>Latest News</h3>
-            </header>
-            <main>
-                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
-                    a user clicks on an element.
-                    To learn more about programming events, please visit our JavaScript tutorial.
-                    Below are the global</p>
-            </main>
-            <footer>
-                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
-                    a user clicks on an element.
-                    To learn more about programming events, please visit our JavaScript tutorial.
-                    Below are the global</p>
-            </footer>
-        </section>
+    <aside id="asideSection" class="grid-33" class="grid-33 tablet-grid-33 mobile-grid-33"">
+    <section id="article1Side">
+        <header><h3>Latest News</h3>
+        </header>
+        <main>
+            <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
+                a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global</p>
+        </main>
+        <footer>
+            <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
+                a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global</p>
+        </footer>
+    </section>
 
-        <section id="article2Side">
-            <header><h3>Upcoming Events</h3></header>
-            <main>
-                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
-                    a user clicks on an element.
-                    To learn more about programming events, please visit our JavaScript tutorial.
-                    Below are the global</p>
-            </main>
-            <footer>
-                <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript
-                    when a user clicks on an element.
-                    To learn more about programming events, please visit our JavaScript tutorial.
-                    Below are the global</p>
-            </footer>
-        </section>
+    <section id="article2Side">
+        <header><h3>Upcoming Events</h3></header>
+        <main>
+            <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
+                a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global</p>
+        </main>
+        <footer>
+            <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript
+                when a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global</p>
+        </footer>
+    </section>
     </aside>
 </main>
 <!--END OF MAIN -->
