ae45810853fa6536cd5ff8af3bd81cbdb1ddaa17
1417758 <1417758@rgu.ac.uk>
2016-03-07 10:12:25 +0000

    asdgsad

--- PHP/index.php
+++ PHP/index.php
@@ -6,14 +6,24 @@
 <h1>
     <?php
     echo "Hello World";
+    ?>
+</h1>
+<br/>
+
+<h2><?php
 
     //concatenation in php
     echo "Hello," . " " . "world" . "!";
 
-    echo 5*7;
-
-    ?>
-</h1>
+    echo 5 * 7; ?>
+</h2>
+<br/>
+<?php
+    /*variables */
+    $myName = "Rachie";
+    $myAge = 19;
+    echo "My name is: " . $myName . "and my age is: " . $myAge;
+?>
 
 
 </body>
