7c7d2392ce732354c7a109f13930b0d5a9ef06f7
1417758 <1417758@rgu.ac.uk>
2016-03-07 10:05:43 +0000

    asdgsad

---
+++ PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>
+    <?php
+    echo "Hello World";
+
+    //concatenation in php
+    echo "Hello," . " " . "world" . "!";
+
+    echo 5*7;
+
+    ?>
+</h1>
+
+
+</body>
+</html>
