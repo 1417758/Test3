6bd7e119e494f588da6cce5a2bc64167bd101228
1417758 <1417758@rgu.ac.uk>
2016-03-15 09:29:51 +0000

    asdgsad

--- DBs/Login.php
+++ DBs/Login.php
@@ -3,19 +3,27 @@ include("DBConnection.php"); //Establishing connection with our database
 
 if (empty($_POST["username"]) || empty($_POST["password"])) {
     echo "Both fields are required.";
-} else {
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT uid FROM users WHERE username='$username' and password='$password'";
-    $result = mysqli_query($db, $sql);
-
-    {
-        header("location: home.php"); // Redirecting To another Page
-    }else
-    {
-        echo "Incorrect username or password.";
-    }
 }
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT uid FROM users WHERE username='$username' and password='$password'";
+$queryResult = mysqli_query($db, $sql);
+
+    if (mysqli_num_rows($queryResult) == 1) {
+        header("location: home.php"); // Redirecting To another Page
+    } else {
+        echo "Incorrect username or password!";
+        echo date('h:i:s') . "<br>";
+
+    //sleep for 5 seconds
+        sleep(5);
+
+    //start again
+        echo date('h:i:s');
+        echo "Try logging in again!";
+        header("location: index.php");
+    }
+
 ?>
--- DBs/home.php
+++ DBs/home.php
@@ -5,6 +5,6 @@
     <link rel="stylesheet" href="~/CSS/htmlDefault.css" type="text/css" />
 </head>
 <body>
-<h1>Hello</h1>
+<h1>Hello you have successfully logged into the database</h1>
 </body>
 </html>
