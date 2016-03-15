9cc9e68e7b3c0223169e60d4e01de0acff33cb34
1417758 <1417758@rgu.ac.uk>
2016-03-15 10:01:54 +0000

    asdgsad

--- DBs/Login.php
+++ DBs/Login.php
@@ -17,7 +17,7 @@ if (mysqli_num_rows($queryResult) == 1) {
     header("location: home.php"); // Redirecting To another Page
 } else {
     echo "Incorrect username or password!\n\n\n";
-    $d_start = strtotime(time,now);
+    $d_start = strtotime(time, now);
     echo date('h:i:s') . "<br>";
 
     //sleep for 15 seconds
@@ -26,15 +26,15 @@ if (mysqli_num_rows($queryResult) == 1) {
     sleep(15);
 
     //start again
-    $d_end = strtotime(time,now);
+    $d_end = strtotime(time, now);
     echo date('h:i:s') . "\n\n";
-    echo "Did you wait 15 secs?" . $d_end - $d_start ;
+    echo "Did you wait 15 secs?" . $d_end - $d_start;
     ob_end_flush();
     // header("location: index.php");
 }
 
 ?>
-    <form method="post" action="index.php">
-       <input type="submit" name="submit" value="index"/>
-    </form>
+<form method="post" action="index.php">
+    <input type="submit" name="submit" value="index"/>
+</form>
 
