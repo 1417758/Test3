e60deafaa25518ce8dbbd413f86f6cd4958184dd
1417758 <1417758@rgu.ac.uk>
2016-03-15 09:38:57 +0000

    asdgsad

--- DBs/Login.php
+++ DBs/Login.php
@@ -4,6 +4,8 @@ include("DBConnection.php"); //Establishing connection with our database
 if (empty($_POST["username"]) || empty($_POST["password"])) {
     echo "Both fields are required.";
 }
+
+echo "you are here! login page";
 $username = $_POST['username'];
 $password = $_POST['password'];
 
