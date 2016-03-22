9d756dacf010ec42bf9b95c3aabf77ca27e831a3
1417758 <1417758@rgu.ac.uk>
2016-03-22 10:03:12 +0000

    asdgsad

--- Heros/DBConnection.php
+++ Heros/DBConnection.php
@@ -1,4 +1,5 @@
 <?php
+/*
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'b592f05b3b6e2e');
 define('DB_PASSWORD', '4e021d86');
@@ -10,5 +11,16 @@ $db2 = mysqli_connect('Database=acsm_0c1b561097cfd9e;Data Source=ap-cdbr-azure-e
 if (mysqli_connect_errno())
 {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+*/
+$db = new mysqli(
+    "ap-cdbr-azure-east-c.cloudapp.net",
+    "b592f05b3b6e2e",
+    "4e021d86",
+    "MySqlDB"
+);
+// test if connection was established, and print any errors
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
 }
 ?>
