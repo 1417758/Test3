a0c38abc29c9563059ceff0f39b786462e34a96a
1417758 <1417758@rgu.ac.uk>
2016-03-22 10:12:05 +0000

    miketest1

--- Heros/DBConnection.php
+++ Heros/DBConnection.php
@@ -1,26 +1,17 @@
 <?php
-/*
-define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME', 'b592f05b3b6e2e');
-define('DB_PASSWORD', '4e021d86');
-define('DB_DATABASE', 'MySqlDB');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-$db2 = mysqli_connect('Database=acsm_0c1b561097cfd9e;Data Source=ap-cdbr-azure-east-c.cloudapp.net;User Id=b592f05b3b6e2e;Password=4e021d86');
-
-// test if connection was established, and print any errors
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-*/
+/**
+ * Created by PhpStorm.
+ * User: michaelcrabb
+ * Date: 18/11/2015
+ * Time: 09:43
+ */
 $db = new mysqli(
-    "ap-cdbr-azure-east-c.cloudapp.net",
-    "b592f05b3b6e2e",
-    "4e021d86",
-    "acsm_0c1b561097cfd9e"
+    "eu-cdbr-azure-west-c.cloudapp.net",
+    "b0c2ff384f05bb",
+    "82b4d3a0",
+    "mjc7778db"
 );
 // test if connection was established, and print any errors
 if (!$db) {
     die('Connect Error: ' . mysqli_connect_errno());
 }
-?>
