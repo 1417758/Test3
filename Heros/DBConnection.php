03c440b9ce8082229f9d6f2b3d7f832928ea6ff5
1417758 <1417758@rgu.ac.uk>
2016-03-22 09:25:42 +0000

    asdgsad

---
+++ Heros/DBConnection.php
@@ -0,0 +1,15 @@
+<?php
+// connect to csdm-webdev server and select database
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b592f05b3b6e2e');
+define('DB_PASSWORD', '4e021d86');
+define('DB_DATABASE', 'acsm_0c1b561097cfd9e');
+
+$db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$db2 = mysqli_connect('Database=acsm_0c1b561097cfd9e;Data Source=ap-cdbr-azure-east-c.cloudapp.net;User Id=b592f05b3b6e2e;Password=4e021d86');
+
+// test if connection was established, and print any errors
+if ($db -> connect_errno) {
+    die('Connectfailed['.$db -> connect_error.']');
+}
+?>
