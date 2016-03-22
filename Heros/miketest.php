c4cd442a5c0fadf7cf09a217419b7ed8fb16925b
1417758 <1417758@rgu.ac.uk>
2016-03-22 10:30:03 +0000

    miketest1

--- Heros/miketest.php
+++ Heros/miketest.php
@@ -5,18 +5,16 @@
  * Date: 22/03/2016
  * Time: 10:05
  */
-include ("DBConnection.php");
+include("DBConnection.php");
 "<br/>";
 echo "Mike Test5";
 
 $sql_query = "SELECT title FROM marvelmovies";
 $result = $db2->query($sql_query);
-while($row = $result->fetch_array())
-{
+while ($row = $result->fetch_array()) {
     $a = $row['title'];
 
-    echo " <h3> {$a} </h3>
-          ";
+    echo " <h3> {$a} </h3> ";
 }
 
 $sql = "SELECT uid FROM users WHERE username='$username' and password='$password'";
