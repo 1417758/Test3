026de5297fbd0ccf12ce312f0b5fbee29e36681b
1417758 <1417758@rgu.ac.uk>
2016-03-22 09:52:46 +0000

    asdgsad

--- Heros/DBConnection.php
+++ Heros/DBConnection.php
@@ -9,7 +9,7 @@ $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 $db2 = mysqli_connect('Database=acsm_0c1b561097cfd9e;Data Source=ap-cdbr-azure-east-c.cloudapp.net;User Id=b592f05b3b6e2e;Password=4e021d86');
 
 // test if connection was established, and print any errors
-if ($db -> connect_errno) {
+if ($db -> connect_error) {
     die('Connectfailed['.$db -> connect_error.']');
 }
 ?>
