915b72688f2d8949d514ad5bd7d176a007ec276d
1417758 <1417758@rgu.ac.uk>
2016-03-22 10:09:28 +0000

    miketest1

--- Heros/miketest.php
+++ Heros/miketest.php
@@ -5,6 +5,19 @@
  * Date: 22/03/2016
  * Time: 10:05
  */
-echo "Mike Test!";
+include ("DBConnection.php");
+echo "Mike Test!!";
+
+$sql_query = "SELECT * FROM marvelmovies";
+$result = $db->query($sql_query);
+while($row = $result->fetch_array())
+{
+    $a = $row['title'];
+
+    echo "<article>
+            <h3> {$a} </h3>
+          </article>";
+}
+?>
 
 ?>
