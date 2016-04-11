7fadf880d2b92cf8039efcc765ac5d8120f0dadd
1417758 <1417758@rgu.ac.uk>
2016-04-11 10:15:20 +0100

    miketest1

---
+++ Sessions/Login.php
@@ -0,0 +1,34 @@
+<?php
+//echo "you are here1! login page";
+include("../Heros/DBConnection.php"); //Establishing connection with our database
+//echo "you are here2! login page";
+if (empty($_POST["username"]) || empty($_POST["password"])) {
+    echo "Both fields are required.";
+}
+
+//echo "you are here3! login page";
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+"<br/>";
+echo "Test here area 51";
+echo $username . "   " . $password;
+
+$sql_query = "SELECT * FROM marvelmovies";
+$result = $db1->query($sql_query);
+
+while ($row = $result->fetch_array()){
+    $a = $row['title'];
+    $b = $row['yearReleased'];
+
+    echo "<p>{$a}</p>";
+    echo "<h3>" . $b . "</h3>";
+}
+
+
+
+?>
+<form method="post" action="index.php">
+    <input type="submit" name="submit" value="Try Again"/>
+</form>
+
