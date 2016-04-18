436996de3ccccd6a82e5bd2c1a3d022abf641a31
1417758 <1417758@rgu.ac.uk>
2016-04-18 12:34:40 +0100

    miketest1

--- PHP/index.php
+++ PHP/index.php
@@ -64,6 +64,10 @@ print $round; // prints 3
 // This time, round pi to 4 places
 $round_decimal = round(M_PI, 4);
 print $round_decimal; // prints 3.1416
+// prints a number between 0 and 32767
+print rand();
+// prints a number between 1 and 10
+print rand(1,10);
 
 ?>
 
