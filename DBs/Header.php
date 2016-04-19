559594be23dd596d9836a9360408d34dfc71807c
1417758 <1417758@rgu.ac.uk>
2016-04-19 11:37:08 +0100

    miketest1

--- DBs/Header.php
+++ DBs/Header.php
@@ -12,18 +12,34 @@
 <body>
 <!--START OF HEADER -->
 <header>
+    <?php
+    //echo "you are here3! login page";
+    $user = $_POST['username'];
+    ?>
+
     <section>
         <h1>Hello!! You have successfully logged into the database</h1>
     </section>
+
+    <marquee>
+        <b>
+            <time datetime="YYYY-MM-DDThh:mm:ssTZD">time</time>
+        </b>
+    </marquee>
 
     <ul id="mainNav">
         <li><a href="...php">All Movies</a></li>
         <li><a href="...php">Movies by Marvel Studios</a></li>
         <li><a href="...php">Movies after 2010</a></li>
         <li><a href="...php">X-Men Movies</a></li>
+        <ul style="float:right;list-style-type:none;display:inline">
+            <li> <b>
+                Hi <?php echo $user; ?>
+                <br/><a href="...">Logout</a>
+                </b>
+            </li>
+        </ul>
     </ul>
-
-
 
 
 </header>
