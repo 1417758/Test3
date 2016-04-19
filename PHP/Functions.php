0e9e6119c42f4d8cbc3ed2c6fbb511050095c7a3
1417758 <1417758@rgu.ac.uk>
2016-04-19 10:52:18 +0100

    miketest1

--- PHP/Functions.php
+++ PHP/Functions.php
@@ -18,11 +18,11 @@ function squareValue2($number) {
 //my other function
 function CaptureAward($name, $numbSpecs, $numbMugs, $numbSausRolls) {
     $award = 10* squareValue2($numbSpecs*$numbMugs*$numbSausRolls)/2;
-    $awardTxt = strtoupper("Wanted "). $name ."/n";
-    $awardTxt .= "Known to be in possession of the following items: /n";
-    $awardTxt .= $numbSpecs . " Specs /n";
-    $awardTxt .= $numbMugs . " Mugs /n";
-    $awardTxt .= $numbSausRolls . " Sausage Rolls /n";
+    $awardTxt = strtoupper("Wanted "). $name ."\n";
+    $awardTxt .= "Known to be in possession of the following items: \n";
+    $awardTxt .= $numbSpecs . " Specs \n";
+    $awardTxt .= $numbMugs . " Mugs \n";
+    $awardTxt .= $numbSausRolls . " Sausage Rolls \n";
     $awardTxt .= "Award for capture: £ " . $award;
     return $awardTxt;
 }
