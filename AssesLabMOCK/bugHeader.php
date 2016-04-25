27a798f533a5ce09e0a777f5f5a2a82f10d08ef6
1417758 <1417758@rgu.ac.uk>
2016-04-25 09:36:53 +0100

    wertwertwat

---
+++ AssesLabMOCK/bugHeader.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>RGU</title>
+    <!--CSSs link -->
+    <link rel="stylesheet" type="text/css" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link rel="stylesheet" type="text/css" href="assets/htmlDefault.css">
+    <link rel="stylesheet" type="text/css" href="assets/menu.css">
+    <link rel="stylesheet" type="text/css" href="assets/rguStyle.css">
+
+</head>
+<body>
+<!--START OF HEADER -->
+<header>
+
+    <section>
+        <img id="figHeader" src="assets/logo.png" alt="logo">
+    </section>
+
+    <section>
+        <h1>Bug Tracker</h1>
+        <h2>Keeping track of all the pesky little bugs</h2>
+    </section>
+
+
+
+</header>
+<!--END OF HEADER -->
---
+++ AssesLabMOCK/bugNavig.php
@@ -0,0 +1,12 @@
+
+
+<ul id="mainNav">
+    <li><a href="...php">Home</a></li>
+    <li><a href="...php">About</a></li>
+    <li><a href="...php">News</a></li>
+    <li><a href="...php">Events</a></li>
+    <li><a href="...php">Staff</a></li>
+    <li><a href="...php">Research</a></li>
+    <li><a href="....php">Study Info</a></li>
+    <li><a href="...php">Industry</a></li>
+</ul>
