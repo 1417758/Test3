48c37e27e11c65131288c2b0e1154ab19e4a6aa8
1417758 <1417758@rgu.ac.uk>
2016-04-25 10:09:18 +0100

    wertwertwat

--- AssesLabMOCK/bugHeader.php
+++ AssesLabMOCK/bugHeader.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" type="text/css" href="assets/unsemantic-grid-responsive-tablet.css">
     <link rel="stylesheet" type="text/css" href="assets/htmlDefault.css">
     <link rel="stylesheet" type="text/css" href="assets/bugMenu.css">
+    <link rel="stylesheet" type="text/css" href="assets/bugStyle.css">
 
 </head>
 <body>
