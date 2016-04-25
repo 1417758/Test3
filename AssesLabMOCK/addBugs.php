6c2cfe217e47070903f9ef83cf6d97bd32c821fb
1417758 <1417758@rgu.ac.uk>
2016-04-25 11:46:44 +0100

    asdfasdf

--- AssesLabMOCK/addBugs.php
+++ AssesLabMOCK/addBugs.php
@@ -10,25 +10,26 @@ include("bugHeader.php");
     </aside>
 
     <section id="mainSection" class="grid-60 tablet-grid-60 mobile-grid-60">
-
-        <table>
-            <tr>
-                <td >Bug Name</td>
-                <td><input type="text" name="txtName" size="75%" placeholder="Bug Name" required></td>
-            </tr>
-            <tr>
-                <td >Bug Summary</td>
-                <td><textarea rows="4" cols="25" name="txtSum" style="width: 445px;" placeholder="Bug Category"></textarea></td>
-            </tr>
-            <tr>
-                <td >Bug Category</td>
-                <td><input type="text" name="txtCat" size="75%" placeholder="Bug Category" required ></td>
-            </tr>
-            <tr>
-                <td colspan="2" ><input type="submit" value="Submit" style="float: right;"></td>
-            </tr>
-        </table>
-
+        <form action="" method="post">
+            <table>
+                <tr>
+                    <td>Bug Name</td>
+                    <td><input type="text" name="txtName" size="75%" placeholder="Bug Name" required></td>
+                </tr>
+                <tr>
+                    <td>Bug Summary</td>
+                    <td><textarea rows="4" cols="25" name="txtSum" style="width: 445px;"
+                                  placeholder="Bug Category"></textarea></td>
+                </tr>
+                <tr>
+                    <td>Bug Category</td>
+                    <td><input type="text" name="txtCat" size="75%" placeholder="Bug Category" required></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="Submit" style="float: right;"></td>
+                </tr>
+            </table>
+        </form>
     </section>
 
 </main>
