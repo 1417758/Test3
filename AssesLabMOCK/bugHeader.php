fa989139395452fa688f54f02d0cfb8a9908ce7a
1417758 <1417758@rgu.ac.uk>
2016-04-25 09:55:52 +0100

    wertwertwat

--- AssesLabMOCK/bugHeader.php
+++ AssesLabMOCK/bugHeader.php
@@ -14,11 +14,11 @@
 <!--START OF HEADER -->
 <header>
 
-    <section>
+    <section class="grid-30 tablet-grid-30 mobile-grid-30">
         <img id="figHeader" src="assets/logo.png" alt="logo">
     </section>
 
-    <section>
+    <section class="grid-60 tablet-grid-60 mobile-grid-60">
         <h1>Bug Tracker</h1>
         <h2>Keeping track of all the pesky little bugs</h2>
     </section>
