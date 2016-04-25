2e052e28210bb83b792e2e7396b494fbfd05357b
1417758 <1417758@rgu.ac.uk>
2016-04-25 11:54:12 +0100

    asdfasdf

--- AssesLabMOCK/bugNavig.php
+++ AssesLabMOCK/bugNavig.php
@@ -1,7 +1,7 @@
 
 
 <ul id="leftNav">
-    <li><a href="showBugs.php">All Gug Items</a></li>
+    <li><a href="showBugs.php">All Bug Items</a></li>
     <li><a href="...php">Android Bugs</a></li>
     <li><a href="...php">iOS Bugs</a></li>
     <li><a href="...php">Windows Bugs</a></li>
--- AssesLabMOCK/showBugs.php
+++ AssesLabMOCK/showBugs.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1417758
- * Date: 25/04/2016
- * Time: 09:27
- */
+include("bugHeader.php");
+?>
+<!--START OF MAIN -->
+<main class="grid-container">
+    <aside id="asideLeft" class="grid-30 tablet-grid-30 mobile-grid-30">
+        <?php
+        include("bugNavig.php");
+        ?>
+    </aside>
+
+    <section id="mainSection" class="grid-60 tablet-grid-60 mobile-grid-60">
+        <?php
+        $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+        echo fread($myfile, filesize("H:/DOCS - MODULES/CMM007 - Intranet Systems Dev/Labs/Test3/AssesLabMOCK/data.txt"));
+        fclose($myfile);
+        ?>
+    </section>
+
+</main>
+<!--END OF MAIN -->
+<?php
+include("rguFooter.php");
+?>
+
