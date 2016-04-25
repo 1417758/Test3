82ebe7a152f1dea0c84248bbb6ca7dd67b4c0dfc
1417758 <1417758@rgu.ac.uk>
2016-04-25 09:25:38 +0100

    wertwertwat

---
+++ AssesLabMOCK/rguDefault.php
@@ -0,0 +1,82 @@
+<?php
+include("rguHeader.php");
+?>
+<!--START OF MAIN -->
+<main class="grid-container">
+
+    <section id="mainSection" class="grid-66 tablet-grid-66 mobile-grid-66">
+        <article id="article1">
+            <img id="wifie" class="grid-33" src="assets/chrisinajayne.jpg">
+
+            <h1>Welcome to the School of Computing and Digital media</h1>
+            <span>Global Event Attributes HTML 4 added the ability to let events trigger actions in a browser, like starting a
+            JavaScript when a
+            user clicks on an element.
+            To learn more about programming events, please visit our JavaScript tutorial.
+            Below are the global event attributes that can be added to HTML elements to define event actions.
+            = New event attributes in HTML5.To learn more about programming events, please visit our JavaScript tutorial.
+            Below are the global event attributes that can be added to HTML elements to define event actions.
+            = New event attributes in HTML5.</span>
+        </article>
+        <article id="article2">
+            <h3>Lorern Ipsum Subittle Text</h3>
+            <span>Global Event Attributes
+                HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a
+                user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global event attributes that can be added to HTML elements to define event actions.
+                = New event attributes in HTML5.</span>
+        </article>
+        <article id="article3">
+            <h3>Lorern Ipsum Subittle Text</h3>
+            <span>Global Event Attributes
+                HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when a
+                user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global event attributes that can be added to HTML elements to define event actions.
+                = New event attributes in HTML5.</span>
+        </article>
+    </section>
+
+    <aside id="asideSection" class="grid-33" class="grid-33 tablet-grid-33 mobile-grid-33"">
+    <section id="article1Side">
+        <header><h3>Latest News</h3>
+        </header>
+        <main>
+            <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
+                a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global</p>
+        </main>
+        <footer>
+            <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
+                a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global</p>
+        </footer>
+    </section>
+
+    <section id="article2Side">
+        <header><h3>Upcoming Events</h3></header>
+        <main>
+            <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript when
+                a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global</p>
+        </main>
+        <footer>
+            <p>HTML 4 added the ability to let events trigger actions in a browser, like starting a JavaScript
+                when a user clicks on an element.
+                To learn more about programming events, please visit our JavaScript tutorial.
+                Below are the global</p>
+        </footer>
+    </section>
+    </aside>
+</main>
+
+<br/>
+<!--END OF MAIN -->
+
+<?php
+include("rguFooter.php");
+?>
