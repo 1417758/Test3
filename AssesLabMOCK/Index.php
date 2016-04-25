5178a7428b63c8375b40fcf226185dd061ec600d
1417758 <1417758@rgu.ac.uk>
2016-04-25 09:55:00 +0100

    wertwertwat

--- AssesLabMOCK/Index.php
+++ AssesLabMOCK/Index.php
@@ -3,15 +3,25 @@ include("bugHeader.php");
 ?>
 <!--START OF MAIN -->
 <main class="grid-container">
-    <aside id="asideLeft">
+    <aside id="asideLeft" class="grid-40 tablet-grid-50 mobile-grid-100">
         <?php
         include("bugNavig.php");
         ?>
     </aside>
 
+    <article id="article1" class="grid-50 tablet-grid-50 mobile-grid-100">
+        <h4>Welcome to Bug Article</h4>
+            <p>Global Event Attributes HTML 4 added the ability to let events trigger actions in a browser, like starting a
+            JavaScript when a
+            user clicks on an element.
+            To learn more about programming events, please visit our JavaScript tutorial.
+            Below are the global event attributes that can be added to HTML elements to define event actions.
+            = New event attributes in HTML5.To learn more about programming events, please visit our JavaScript tutorial.
+            Below are the global event attributes that can be added to HTML elements to define event actions.
+            = New event attributes in HTML5.</p>
+    </article>
+
 </main>
-
-
 <!--END OF MAIN -->
 <?php
 include("rguFooter.php");
