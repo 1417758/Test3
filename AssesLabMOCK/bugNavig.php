1d09e8b1230222ec8d88081adc15659b23e89f2a
1417758 <1417758@rgu.ac.uk>
2016-04-25 11:45:32 +0100

    asdfasdf

--- AssesLabMOCK/bugNavig.php
+++ AssesLabMOCK/bugNavig.php
@@ -6,7 +6,6 @@
     <li><a href="...php">iOS Bugs</a></li>
     <li><a href="...php">Windows Bugs</a></li>
     <li><a href="addBugs.php">Insert Bugs</a></li>
-    <li><a href=""></a></li>
     <ul style="float:none;list-style-type:none;display:inline">
         <li><a href="Index.php">Home</a></li>
     </ul>
