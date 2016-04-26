21545ae0e659306409a40590f332bc398e804ee8
1417758 <1417758@rgu.ac.uk>
2016-04-26 13:40:25 +0100

    asdfasdf

--- AssesLabMOCK/assets/Functions.php
+++ AssesLabMOCK/assets/Functions.php
@@ -20,27 +20,27 @@ function addBug($db, $name, $sum, $cat)
 
     // create a SQL query as a string
     $sql_query = "INSERT INTO BUGS VALUES ($count, $name, $sum, $cat)";
-    echo "something after executing sql";
+    echo "something before executing sql";
     // execute the SQL query
-    if (mysqli_query($db, $sql_query)) {
+    if (mysqli_query($db, $sql_query))
         echo "sql query executed succesfully". "<br/>" . mysqli_affected_rows();
-    } else
+    else
         echo "Error: " . $sql_query . "<br/>" . mysqli_error($db);
 
-    $result = $db->query($sql_query);
-    echo "something after executing sql";
-    echo "result: " . $result;
-    // iterate over $result object one $row at a time
-    // use fetch_array() to return an associative array
-    /*while ($row = $result->fetch_array()) {
-        echo "result: ".count($result);
-        echo "row: ".count($row);
-        echo $row;
-        $a = $row['title'];
-        $b = $row['yearReleased'];
-        echo "<p>{$a}</p>";
-        echo "<h3>" . $b . "</h3>";
-    }*/
+    /* $result = $db->query($sql_query);
+     echo "something after executing sql";
+     echo "result: " . $result;
+     // iterate over $result object one $row at a time
+     // use fetch_array() to return an associative array
+   /*while ($row = $result->fetch_array()) {
+         echo "result: ".count($result);
+         echo "row: ".count($row);
+         echo $row;
+         $a = $row['title'];
+         $b = $row['yearReleased'];
+         echo "<p>{$a}</p>";
+         echo "<h3>" . $b . "</h3>";
+     }*/
 
     //increment counter
     $GLOBALS['count']++;
