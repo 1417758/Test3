ef3478d2583dabd8b8e79e85223a065976d3f0e8
1417758 <1417758@rgu.ac.uk>
2016-04-26 12:08:29 +0100

    asdfasdf

--- AssesLabMOCK/assets/Functions.php
+++ AssesLabMOCK/assets/Functions.php
@@ -3,10 +3,56 @@
 //function
 function PrintBug($name, $cat, $sum)
 {
-
-    print("Bug Name: " . $name ). "<br>";
-    print("Bug Category: " . $cat ). "<br>";
-    print("Bug Summary: " . $sum ). "<br>";
+    print("Bug Name: " . $name) . "<br>";
+    print("Bug Category: " . $cat) . "<br>";
+    print("Bug Summary: " . $sum) . "<br>";
     print "<hr>";
+}
 
+function addBug($db, $name, $sum, $cat)
+{
+    // create a SQL query as a string
+    $sql_query = "INSERT INTO BUGS VALUES ($name, $sum, $cat)";
+    // execute the SQL query
+    $result = $db->query($sql_query);
+
+    // iterate over $result object one $row at a time
+    // use fetch_array() to return an associative array
+    while ($row = $result->fetch_array()) {
+
+        echo $row;
+        /*$a = $row['title'];
+        $b = $row['yearReleased'];
+        echo "<p>{$a}</p>";
+        echo "<h3>" . $b . "</h3>";*/
+    }
+}
+
+
+function getBugs($db)
+{
+    $sql_query = "SELECT * FROM marvelmovies";
+    $result = $db->query($sql_query);
+
+    while ($row = $result->fetch_array()) {
+        $a = $row['title'];
+        $b = $row['yearReleased'];
+
+        echo "<p>{$a}</p>";
+        echo "<h3>" . $b . "</h3>";
+    }
+}
+
+function getBugs2($db)
+{
+    $sql_query = "SELECT * FROM marvelmovies";
+    $result = $db->query($sql_query);
+
+    while ($row = $result->fetch_array()) {
+        $a = $row['title'];
+        $b = $row['yearReleased'];
+
+        echo "<p>{$a}</p>";
+        echo "<h3>" . $b . "</h3>";
+    }
 }
--- AssesLabMOCK/showBugs.php
+++ AssesLabMOCK/showBugs.php
@@ -10,29 +10,22 @@ include("bugHeader.php");
     </aside>
 
     <section id="mainSection" class="grid-60 tablet-grid-60 mobile-grid-60">
+
         <?php
-        $myfile = fopen("assets/data.txt", "r") or die("Unable to open file!");
-        // Output one line until end-of-file
-        while (!feof($myfile)) {
-            echo fgets($myfile) . "<br>";
-        }
-        fclose($myfile);
-        echo "<hr>";
+        //get data entered by user on insert bug page";
+        $a = $_GET['txtName'];
+        $b = $_GET['txtSum'];
+        $c = $_GET['txtCat'];
 
         //add data from addBugs.php page
-        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            // loop through the superglobal POST array
-            foreach ($_GET as $key => $value) {
-                print("key: " . $key . " value: " . $value);
-            }
-            echo "<hr>";
+        if (!empty($a) AND $_SERVER['REQUEST_METHOD'] === 'GET') {
+            //ADD DATA TO DB
+            addBug($db1, $a, $b, $c);
+
+            //display all bugs
+           // getBugs($db1);
         }
 
-        //echo "you are here3! login page";
-        $a = $_GET['txtName'];
-        $b = $_GET['txtCat'];
-        $c = $_GET['txtSum'];
-        PrintBug($a, $b, $c);
 
         ?>
     </section>
