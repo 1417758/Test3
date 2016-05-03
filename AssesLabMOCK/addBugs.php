88c88ab79dea709564bc7f92640447a60bd6b4b7
1417758 <1417758@rgu.ac.uk>
2016-05-03 16:50:20 +0100

    asdfasdf

--- AssesLabMOCK/addBugs.php
+++ AssesLabMOCK/addBugs.php
@@ -11,63 +11,61 @@ include("bugHeader.php");
 
     <section id="mainSection" class="grid-60 tablet-grid-60 mobile-grid-60">
 
-        <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') { ?>
 
-            <form action="<? echo $_SERVER["PHP_SELF"] ?>"
-            <!-- action="showBugs.php" method="get" -->
-            <table>
-                <tr>
-                    <td>Bug Name</td>
-                    <td><input type="text" name="txtName" size="72%" placeholder="Bug Name" required></td>
-                </tr>
-                <tr>
-                    <td>Bug Summary</td>
-                    <td><textarea rows="4" cols="25" name="txtSum" style="width: 445px;"
-                                  placeholder="Bug Category" required></textarea></td>
-                </tr>
-                <tr>
-                    <td>Bug Category</td>
-                    <td>
-                        <select name="txtCat" required style="width: 445px;">
-                            <option value='Android'>Android</option>
-                            <option value='iOS'>iOS</option>
-                            <option value='Windows'>Windows</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2"><input type="submit" value="Submit" style="float: right;"></td>
-                </tr>
-            </table>
-            </form>
+        <form action="<? echo $_SERVER["PHP_SELF"] ?>"
+        <!-- action="showBugs.php" method="get" -->
+        <table>
+            <tr>
+                <td>Bug Name</td>
+                <td><input type="text" name="txtName" size="72%" placeholder="Bug Name" required></td>
+            </tr>
+            <tr>
+                <td>Bug Summary</td>
+                <td><textarea rows="4" cols="25" name="txtSum" style="width: 445px;"
+                              placeholder="Bug Category" required></textarea></td>
+            </tr>
+            <tr>
+                <td>Bug Category</td>
+                <td>
+                    <select name="txtCat" required style="width: 445px;">
+                        <option value='Android'>Android</option>
+                        <option value='iOS'>iOS</option>
+                        <option value='Windows'>Windows</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="Submit" style="float: right;"></td>
+            </tr>
+        </table>
+        </form>
 
 
         <?
-    // loop through the superglobal GET array
-    foreach ($_GET as $key => $value){
-        PrintKeyAndValue($key, $value);
-    }
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            // loop through the superglobal GET array
+            foreach ($_GET as $key => $value) {
+                PrintKeyAndValue($key, $value);
+            }
 
-}
-//otherwise print Post stuff (data entered to form)
-elseif ($_SERVER['REQUEST_METHOD'] === 'POST')
-{
-    // loop through the superglobal POST array
-    /*foreach ($_POST as $key => $value){
-        PrintKeyAndValue($key, $value);
-    }*/
-    //get data entered by user on insert bug page";
-        $name = $_POST['txtName'];
-        $summary = $_POST['txtSum'];
-        $category = $_POST['txtCat'];
-        //ADD DATA TO DB
-        addBug($db1, $name, $summary, $category);
-        //redirects to showBugs page
-        header('Location: showBugs.php');
+        } //otherwise print Post stuff (data entered to form)
+        elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // loop through the superglobal POST array
+            /*foreach ($_POST as $key => $value){
+                PrintKeyAndValue($key, $value);
+            }*/
+            //get data entered by user on insert bug page";
+            $name = $_POST['txtName'];
+            $summary = $_POST['txtSum'];
+            $category = $_POST['txtCat'];
+            //ADD DATA TO DB
+            addBug($db1, $name, $summary, $category);
+            //redirects to showBugs page
+            header('Location: showBugs.php');
 
-}else
-      //redirects to index page
-        header('Location: Index.php');
+        } else
+            //redirects to index page
+            header('Location: Index.php');
 
 
         ?>
