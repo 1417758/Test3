8df55341af5419ee6536e57148ec4ec035b61c4e
1417758 <1417758@rgu.ac.uk>
2016-05-03 15:42:03 +0100

    asdfasdf

--- AssesLabMOCK/bugNavig.php
+++ AssesLabMOCK/bugNavig.php
@@ -3,9 +3,9 @@
 <ul id="leftNav">
     <li><a href="showBugs.php">All Bug Items</a></li>
     <li><a href="showBugs2.php">All Bug Items (mine)</a></li>
-    <li><a href="...php">Android Bugs</a></li>
-    <li><a href="...php">iOS Bugs</a></li>
-    <li><a href="...php">Windows Bugs</a></li>
+    <li><a href="showBugs.php">Android Bugs</a></li>
+    <li><a href="showBugs.php">iOS Bugs</a></li>
+    <li><a href="showBugs.php">Windows Bugs</a></li>
     <li><a href="addBugs.php">Insert Bugs</a></li>
     <ul style="float:none;list-style-type:none;display:inline">
         <li><a href="Index.php">Home</a></li>
